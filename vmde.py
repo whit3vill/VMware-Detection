34f19cf7e23948a6642035175339e25d0444f8bc
whit3vill <31686076+whit3vill@users.noreply.github.com>
2023-02-23 09:14:24 +0900

    Update vmde.py

--- vmde.py
+++ vmde.py
@@ -1,1 +1,48 @@
 import os
+from operation impmort *
+
+def main():
+    print "STARTING DIAGNOSIS"
+    print "\n"
+    print "  Distribution : ", open("/proc/sys/kernel/ostype").read()
+    print "  OS: ", open("/etc/lsb-release").read()[85:103]
+    print "  Kernel Version :", open("/proc/sys/kernel/osrelease").read()
+    print "  Host name: ",open("/proc/sys/kernel/hostname").read()
+    
+    print "  Presence of Virtual Machine: "
+	presence()
+	print "\n"
+	
+	print "Checking VM Elements"
+	
+	print "\t Hypervisor Flag"
+	flags()
+	
+	print "\t Checking SCSI "
+	scsi()
+	
+	print "\t Checking MAC Address "
+	mac()
+	
+	print "\t Checking bi0s Vendor "
+	bios_vendor()
+	
+	print "\t Checking Product Name "
+	product_vendor()
+	
+	print "\t Checking System Vendor "
+	sys_vendor()
+	
+	print "\t Checking Board Vendor "
+	board_vendor()
+	
+	print "\t Checking Loadable Kernel Modules "
+	kernel_modules()
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
